53aba392e8611a79b80b9da45f85c90ef12ba6ed
lorenmh <neutronstar@gmail.com>
2017-04-18 10:04:02 -0700

    segfault

--- main.c
+++ main.c
@@ -17,6 +17,7 @@ int main(int argc, char *argv[]) {
   }
 
   struct addrinfo hints;
+  struct addrinfo *results;
   struct addrinfo *result;
 
   // clear out hints
@@ -28,9 +29,58 @@ int main(int argc, char *argv[]) {
   hints.ai_family = AF_UNSPEC;
   hints.ai_socktype = SOCK_DGRAM;
 
-  int err = getaddrinfo(HOSTNAME, PORT, &hints, &result);
+  int err = getaddrinfo(HOSTNAME, PORT, &hints, &results);
 
   if (err) {
     fprintf(stderr, "Error invoking getaddrinfo: %s\n", strerror(errno));
   }
+
+    // char hostname[NI_MAXHOST];
+
+    // err = getnameinfo(
+    //   result->ai_addr,
+    //   result->ai_addrlen,
+    //   hostname,
+    //   NI_MAXHOST,
+    //   NULL,
+    //   0,
+    //   0
+    // );
+
+    // if (err != 0) {
+    //     fprintf(stderr, "Error invoking getnameinfo: %s\n", strerror(errno));
+    // }
+
+    // printf(
+    //   "Hostname: %s, Port: %d",
+    //   hostname,
+    //   ((struct sockaddr_in*)result)->sin_port
+    // );
+
+  
+  for (result = results; result != NULL; result = result->ai_next) {
+    err = bind(socket_fd, result->ai_addr, result->ai_addrlen);
+
+    if (err) {
+      fprintf(stderr, "Error binding socket: %s\n", strerror(errno));
+    } else {
+      printf("Connected\n");
+    }
+  }
+
+  char buf[1024];
+  socklen_t len = sizeof(result->ai_addr);
+
+  int n = recvfrom(
+    socket_fd,
+    buf,
+    sizeof(buf),
+    0,
+    result->ai_addr,
+    &len
+  );
+
+  printf("After listen\n");
+
+
 }
