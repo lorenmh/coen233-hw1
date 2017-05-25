ca3fe7eda8bcff2492b38d03e69e73663ccb7be2
loren <neutronstar@gmail.com>
2017-05-24 23:36:44 -0700

    mbp

--- protocol.c
+++ protocol.c
@@ -1,73 +1,137 @@
 #include <stdlib.h>
-
 #include "protocol.h"
+
+uint16_t ntohs(uint16_t netshort);
 
 // Assume that buf has been allocated enough space, otherwise segfault
 int resolve_packet(char const *buf, packet* p) {
-  uint8_t client_id;
-  uint16_t type;
-  uint8_t segment_id;
-  uint8_t len = 0;
-  uint8_t *payload = 0;
-  uint8_t reject_id = 0;
-  
-  uint16_t netshort;
+	uint8_t client_id;
+	uint16_t type;
+	uint8_t segment_id;
+	uint8_t len = 0;
+	uint8_t *payload = 0;
+	uint8_t reject_id = 0;
 
-  // check the opening delimiter
-  memcpy(&netshort, buf, 2);
-  if (ntohs(netshort) != DELIMETER) {
-    return ERR_OPEN_DELIMETER;
-  }
+	uint16_t netshort;
 
-  // copy the client id
-  memcpy(&client_id, &buf[2], 1);
+	// check the opening delimiter
+	memcpy(&netshort, buf, 2);
+	if (ntohs(netshort) != DELIMETER) {
+		return ERR_OPEN_DELIMETER;
+	}
 
-  // copy the type
-  memcpy(&netshort, &buf[3], 2);
-  type = ntohs(netshort);
+	// copy the client id
+	memcpy(&client_id, &buf[2], 1);
 
-  // check the type, populate as necessary
-  int close_addr;
-  if (type == ACK) {
-    // copy segment id
-    memcpy(&segment_id, &buf[5], 1);
-    // set close addr to check closing delimiter
-    close_addr = 8;
-  } else if (type == REJECT) {
-    // copy reject id
-    memcpy(&netshort, &buf[5], 2);
-    reject_id = ntohs(netshort);
-    // copy segment id
-    memcpy(&segment_id, &buf[7], 1);
-    // set close addr to check closing delimiter
-    close_addr = 10;
-  } else if (type == DATA) {
-    // copy segment id
-    memcpy(&segment_id, &buf[5], 1);
-    // copy the length
-    memcpy(&len, &buf[6], 1);
-    // copy the payload
-    payload = malloc(sizeof(uint8_t) * len);
-    memcpy(payload, &buf[7], len);
-    // set close addr to check closing delimiter
-    close_addr = 7 + len;
-  } else {
-    // invalid type
-    return ERR_INVALID_TYPE;
-  }
+	// copy the type
+	memcpy(&netshort, &buf[3], 2);
+	type = ntohs(netshort);
 
-  // check the closing delimiter
-  memcpy(&netshort, &buf[close_addr], 2);
-  if (ntohs(netshort) != DELIMETER) {
-    return ERR_CLOSE_DELIMETER;
-  }
+	// check the type, populate as necessary
+	int close_addr;
+	if (type == ACK) {
+		// copy segment id
+		memcpy(&segment_id, &buf[5], 1);
+		// set close addr to check closing delimiter
+		close_addr = 8;
+	} else if (type == REJECT) {
+		// copy reject id
+		memcpy(&netshort, &buf[5], 2);
+		reject_id = ntohs(netshort);
+		// copy segment id
+		memcpy(&segment_id, &buf[7], 1);
+		// set close addr to check closing delimiter
+		close_addr = 10;
+	} else if (type == DATA) {
+		// copy segment id
+		memcpy(&segment_id, &buf[5], 1);
+		// copy the length
+		memcpy(&len, &buf[6], 1);
+		// copy the payload
+		payload = malloc(sizeof(uint8_t) * len);
+		memcpy(payload, &buf[7], len);
+		// set close addr to check closing delimiter
+		close_addr = 7 + len;
+	} else {
+		// invalid type
+		return ERR_INVALID_TYPE;
+	}
 
-  p->client_id = client_id;
-  p->type = type;
-  p->segment_id = segment_id;
-  p->len = len;
-  p->payload = payload;
-  p->reject_id = reject_id;
+	// check the closing delimiter
+	memcpy(&netshort, &buf[close_addr], 2);
+	if (ntohs(netshort) != DELIMETER) {
+		return ERR_CLOSE_DELIMETER;
+	}
 
-  return 0;
+	p->client_id = client_id;
+	p->type = type;
+	p->segment_id = segment_id;
+	p->len = len;
+	p->payload = payload;
+	p->reject_id = reject_id;
+
+	return 0;
+}
+
+packet_list *create_packet_list() {
+	packet_list *pl = malloc(sizeof(packet_list));
+	packet_list_node *buf = malloc(
+			sizeof(packet_list_node)*PACKET_LIST_INITIAL_SIZE
+	);
+
+	pl->head = NULL;
+	pl->tail = NULL;
+	pl->buf = buf;
+	pl->size = PACKET_LIST_INITIAL_SIZE;
+
+	return pl;
+}
+
+void free_packet(packet *p) {
+	free(p->payload);
+}
+
+void free_packet_list(packet_list *pl) {
+	if (pl->head) {
+		size_t increment = sizeof(packet_list_node);
+		packet_list_node *cursor = pl->buf;
+		do {
+			free_packet(cursor->p);
+			cursor += increment;
+		} while(cursor <= pl->tail);
+	}
+	free(pl->buf);
+	free(pl);
+}
+
+void append(packet_list *pl, packet *p) {
+	size_t packet_size = sizeof(packet);
+	size_t packet_list_node_size = sizeof(packet_list_node);
+
+	// initial list
+	if (pl->tail == NULL) {
+		pl->head = pl->buf;
+		pl->tail = pl->buf;
+	}
+
+	// number of elements in the list
+	size_t num = ((pl->head - pl->tail) / packet_size) + 1;
+
+	// our buffer is full, resize
+	if (num >= pl->size) {
+		packet_list_node *buf = malloc(
+				packet_list_node_size * PACKET_LIST_FACTOR * pl->size
+		);
+
+		memcpy(buf, pl->buf, packet_list_node_size * pl->size);
+
+		free(pl->buf);
+
+		pl->buf = buf;
+		pl->head = buf;
+		pl->tail = buf + packet_list_node_size * pl->size;
+	}
+
+		//memcpy(pl->buf, p, packet_size);
+	//memcpy(tail, 
 }
--- protocol.h
+++ protocol.h
@@ -6,33 +6,60 @@
 #include <sys/socket.h>
 
 #define DELIMETER 0xFFFF
+#define CLIENT_TABLE_SIZE 256
+#define PACKET_LIST_INITIAL_SIZE 16
+#define PACKET_LIST_FACTOR 2
 
 typedef enum packet_t {
-  DATA = 0xFFF1,
-  ACK = 0xFFF2,
-  REJECT = 0xFFF3
+	DATA = 0xFFF1,
+	ACK = 0xFFF2,
+	REJECT = 0xFFF3
 } packet_t;
 
+
 typedef enum reject_t {
-  OUT_OF_SEQ = 0xFFF4,
-  LEN_MISMATCH = 0xFFF5,
-  END_PACKET_MISSING = 0xFFF6,
-  DUP_PACKET = 0xFFF7
+	OUT_OF_SEQ = 0xFFF4,
+	LEN_MISMATCH = 0xFFF5,
+	END_PACKET_MISSING = 0xFFF6,
+	DUP_PACKET = 0xFFF7
 } reject_t;
 
 typedef enum error_t {
-  ERR_OPEN_DELIMETER = 1,
-  ERR_CLOSE_DELIMETER,
-  ERR_INVALID_TYPE
+	ERR_OPEN_DELIMETER = 1,
+	ERR_CLOSE_DELIMETER,
+	ERR_INVALID_TYPE
 } error_t;
 
 typedef struct packet {
-  uint8_t client_id;
-  uint16_t type;
-  uint8_t segment_id;
-  uint8_t len;
-  uint8_t *payload;
-  uint8_t reject_id;
+	uint8_t client_id;
+	uint16_t type;
+	uint8_t segment_id;
+	uint8_t len;
+	uint8_t *payload;
+	uint8_t reject_id;
 } packet;
+
+typedef struct packet_list_node {
+	struct packet_list_node* next;
+	packet* p;
+} packet_list_node;
+
+// linked list to hold packets from a client
+typedef struct packet_list {
+	packet_list_node *head;
+	packet_list_node *tail;
+	packet_list_node *buf;
+	uint16_t size;
+} packet_list;
+
+typedef struct client {
+	packet_t *packet_list;
+} client;
+
+typedef struct client_table {
+	client *table[CLIENT_TABLE_SIZE];
+	size_t size;
+} client_tree;
+
 
 #endif
